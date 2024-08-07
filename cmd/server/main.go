fcfd49d945bbea94c910b1f446cb4a3cecc19763
FormJani <janos.molnar@formlabs.com>
2024-08-08 00:33:47 +0200

    move main to cmd/ folder

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -3,11 +3,13 @@ package main
 import (
 	"log/slog"
 	"os"
+
+	server "github.com/molnarjani/yonash-dev/internal"
 )
 
 func main() {
 	// Run your server.
-	if err := runServer(); err != nil {
+	if err := server.RunServer(); err != nil {
 		slog.Error("Failed to start server!", "details", err.Error())
 		os.Exit(1)
 	}
