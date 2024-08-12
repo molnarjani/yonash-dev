2458f407ec7821034a1c92a6c1f34b760902ab65
FormJani <janos.molnar@formlabs.com>
2024-08-13 00:54:30 +0200

    Add index page content

--- internal/web/templates/components/navbar_templ.go
+++ internal/web/templates/components/navbar_templ.go
@@ -30,7 +30,7 @@ func Logo() templ.Component {
 			templ_7745c5c3_Var1 = templ.NopComponent
 		}
 		ctx = templ.ClearChildren(ctx)
-		_, templ_7745c5c3_Err = templ_7745c5c3_Buffer.WriteString("<!-- Logo --><a href=\"#\" class=\"flex items-left space-x-3 rtl:space-x-reverse block py-2 px-3 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:p-0 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent\"><span class=\"self-center text-xl text-left text-gray-700 text-inherit font-semibold whitespace-nowrap dark:text-white\"><span class=\"blink\">")
+		_, templ_7745c5c3_Err = templ_7745c5c3_Buffer.WriteString("<!-- Logo --><a href=\"/\" class=\"flex items-left space-x-3 rtl:space-x-reverse block py-2 px-3 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:p-0 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent\"><span class=\"self-center text-xl text-left text-gray-700 text-inherit font-semibold whitespace-nowrap dark:text-white\"><span class=\"blink\">")
 		if templ_7745c5c3_Err != nil {
 			return templ_7745c5c3_Err
 		}
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,17 @@ module.exports = {
     './node_modules/flowbite/**/*.js',
   ],
   theme: {
-    extend: {},
+    extend: {
+      keyframes: {
+        fadeIn: {
+          '0%': { opacity: 0 },
+          '100%': { opacity: 1 },
+        },
+      },
+      animation: {
+        'fade-in': 'fadeIn 1.5s ease-out forwards',
+      },
+    },
   },
   plugins: [
     require('@tailwindcss/forms'),
