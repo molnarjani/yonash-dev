80d4ae6ed106112633ca3f9cc64ea87b8bcec338
FormJani <janos.molnar@formlabs.com>
2024-08-16 01:18:22 +0200

    reuse container component for blog and contacts

--- internal/web/templates/pages/blog_templ.go
+++ internal/web/templates/pages/blog_templ.go
@@ -8,6 +8,8 @@ package pages
 import "github.com/a-h/templ"
 import templruntime "github.com/a-h/templ/runtime"
 
+import "github.com/molnarjani/yonash-dev/internal/web/templates/components"
+
 func BlogPage() templ.Component {
 	return templruntime.GeneratedTemplate(func(templ_7745c5c3_Input templruntime.GeneratedComponentInput) (templ_7745c5c3_Err error) {
 		templ_7745c5c3_W, ctx := templ_7745c5c3_Input.Writer, templ_7745c5c3_Input.Context
@@ -26,7 +28,25 @@ func BlogPage() templ.Component {
 			templ_7745c5c3_Var1 = templ.NopComponent
 		}
 		ctx = templ.ClearChildren(ctx)
-		_, templ_7745c5c3_Err = templ_7745c5c3_Buffer.WriteString("<div class=\"bg-white\"><div class=\"container mx-auto py-8\"><div class=\"col-span-4 sm:col-span-9\"><div class=\"py-8 lg:py-16 px-4 mx-auto max-w-screen-md\"><p class=\"mb-4 lg:mb-8 font-light text-center text-gray-700 dark:text-gray-400 sm:text-xl\">No posts here yet, check back later...</p></div><section class=\"bg-white dark:bg-gray-900 flex justify-center items-center\"><div role=\"status\" class=\"max-w-md p-4 space-y-4 border border-gray-200 divide-y divide-gray-200 rounded shadow animate-pulse dark:divide-gray-700 md:p-6 dark:border-gray-700\"><div class=\"flex items-center justify-between\"><div><div class=\"h-2.5 bg-gray-300 rounded-full dark:bg-gray-600 w-24 mb-2.5\"></div><div class=\"w-32 h-2 bg-gray-200 rounded-full dark:bg-gray-700\"></div></div><div class=\"h-2.5 bg-gray-300 rounded-full dark:bg-gray-700 w-12\"></div></div><div class=\"flex items-center justify-between pt-4\"><div><div class=\"h-2.5 bg-gray-300 rounded-full dark:bg-gray-600 w-24 mb-2.5\"></div><div class=\"w-32 h-2 bg-gray-200 rounded-full dark:bg-gray-700\"></div></div><div class=\"h-2.5 bg-gray-300 rounded-full dark:bg-gray-700 w-12\"></div></div><div class=\"flex items-center justify-between pt-4\"><div><div class=\"h-2.5 bg-gray-300 rounded-full dark:bg-gray-600 w-24 mb-2.5\"></div><div class=\"w-32 h-2 bg-gray-200 rounded-full dark:bg-gray-700\"></div></div><div class=\"h-2.5 bg-gray-300 rounded-full dark:bg-gray-700 w-12\"></div></div><div class=\"flex items-center justify-between pt-4\"><div><div class=\"h-2.5 bg-gray-300 rounded-full dark:bg-gray-600 w-24 mb-2.5\"></div><div class=\"w-32 h-2 bg-gray-200 rounded-full dark:bg-gray-700\"></div></div><div class=\"h-2.5 bg-gray-300 rounded-full dark:bg-gray-700 w-12\"></div></div><div class=\"flex items-center justify-between pt-4\"><div><div class=\"h-2.5 bg-gray-300 rounded-full dark:bg-gray-600 w-24 mb-2.5\"></div><div class=\"w-32 h-2 bg-gray-200 rounded-full dark:bg-gray-700\"></div></div><div class=\"h-2.5 bg-gray-300 rounded-full dark:bg-gray-700 w-12\"></div></div><span class=\"sr-only\">Loading...</span></div></section></div></div></div>")
+		templ_7745c5c3_Var2 := templruntime.GeneratedTemplate(func(templ_7745c5c3_Input templruntime.GeneratedComponentInput) (templ_7745c5c3_Err error) {
+			templ_7745c5c3_W, ctx := templ_7745c5c3_Input.Writer, templ_7745c5c3_Input.Context
+			templ_7745c5c3_Buffer, templ_7745c5c3_IsBuffer := templruntime.GetBuffer(templ_7745c5c3_W)
+			if !templ_7745c5c3_IsBuffer {
+				defer func() {
+					templ_7745c5c3_BufErr := templruntime.ReleaseBuffer(templ_7745c5c3_Buffer)
+					if templ_7745c5c3_Err == nil {
+						templ_7745c5c3_Err = templ_7745c5c3_BufErr
+					}
+				}()
+			}
+			ctx = templ.InitializeContext(ctx)
+			_, templ_7745c5c3_Err = templ_7745c5c3_Buffer.WriteString("<p class=\"mb-4 lg:mb-8 font-light text-center text-gray-700 dark:text-gray-400 sm:text-xl\">No posts here yet, check back later...</p><section class=\"bg-white dark:bg-gray-900 flex justify-center items-center\"><div role=\"status\" class=\"max-w-md p-4 space-y-4 border border-gray-200 divide-y divide-gray-200 rounded shadow animate-pulse dark:divide-gray-700 md:p-6 dark:border-gray-700\"><div class=\"flex items-center justify-between\"><div><div class=\"h-2.5 bg-gray-300 rounded-full dark:bg-gray-600 w-24 mb-2.5\"></div><div class=\"w-32 h-2 bg-gray-200 rounded-full dark:bg-gray-700\"></div></div><div class=\"h-2.5 bg-gray-300 rounded-full dark:bg-gray-700 w-12\"></div></div><div class=\"flex items-center justify-between pt-4\"><div><div class=\"h-2.5 bg-gray-300 rounded-full dark:bg-gray-600 w-24 mb-2.5\"></div><div class=\"w-32 h-2 bg-gray-200 rounded-full dark:bg-gray-700\"></div></div><div class=\"h-2.5 bg-gray-300 rounded-full dark:bg-gray-700 w-12\"></div></div><div class=\"flex items-center justify-between pt-4\"><div><div class=\"h-2.5 bg-gray-300 rounded-full dark:bg-gray-600 w-24 mb-2.5\"></div><div class=\"w-32 h-2 bg-gray-200 rounded-full dark:bg-gray-700\"></div></div><div class=\"h-2.5 bg-gray-300 rounded-full dark:bg-gray-700 w-12\"></div></div><div class=\"flex items-center justify-between pt-4\"><div><div class=\"h-2.5 bg-gray-300 rounded-full dark:bg-gray-600 w-24 mb-2.5\"></div><div class=\"w-32 h-2 bg-gray-200 rounded-full dark:bg-gray-700\"></div></div><div class=\"h-2.5 bg-gray-300 rounded-full dark:bg-gray-700 w-12\"></div></div><div class=\"flex items-center justify-between pt-4\"><div><div class=\"h-2.5 bg-gray-300 rounded-full dark:bg-gray-600 w-24 mb-2.5\"></div><div class=\"w-32 h-2 bg-gray-200 rounded-full dark:bg-gray-700\"></div></div><div class=\"h-2.5 bg-gray-300 rounded-full dark:bg-gray-700 w-12\"></div></div><span class=\"sr-only\">Loading...</span></div></section>")
+			if templ_7745c5c3_Err != nil {
+				return templ_7745c5c3_Err
+			}
+			return templ_7745c5c3_Err
+		})
+		templ_7745c5c3_Err = components.Container().Render(templ.WithChildren(ctx, templ_7745c5c3_Var2), templ_7745c5c3_Buffer)
 		if templ_7745c5c3_Err != nil {
 			return templ_7745c5c3_Err
 		}
--- internal/web/templates/pages/contacts_templ.go
+++ internal/web/templates/pages/contacts_templ.go
@@ -8,9 +8,7 @@ package pages
 import "github.com/a-h/templ"
 import templruntime "github.com/a-h/templ/runtime"
 
-import (
-	"github.com/molnarjani/yonash-dev/internal/web/templates/components"
-)
+import "github.com/molnarjani/yonash-dev/internal/web/templates/components"
 
 func ContactsPage() templ.Component {
 	return templruntime.GeneratedTemplate(func(templ_7745c5c3_Input templruntime.GeneratedComponentInput) (templ_7745c5c3_Err error) {
@@ -30,15 +28,33 @@ func ContactsPage() templ.Component {
 			templ_7745c5c3_Var1 = templ.NopComponent
 		}
 		ctx = templ.ClearChildren(ctx)
-		_, templ_7745c5c3_Err = templ_7745c5c3_Buffer.WriteString("<div class=\"bg-white\"><div class=\"container mx-auto py-8\"><div class=\"col-span-4 sm:col-span-9\"><section class=\"bg-white dark:bg-gray-900\"><div class=\"py-8 lg:py-16 px-4 mx-auto max-w-screen-md\"><p class=\"mb-4 lg:mb-8 font-light text-center text-gray-700 dark:text-gray-400 sm:text-xl\">feel free to reach out to me for work related offers or just to have a chat :)</p><div class=\"flex flex-col items-center\"><p class=\"mb-4 lg:mb-8 font-light text-center text-gray-500 dark:text-gray-400 sm:text-xl\">find me on my socials</p><!-- social icons -->")
-		if templ_7745c5c3_Err != nil {
+		templ_7745c5c3_Var2 := templruntime.GeneratedTemplate(func(templ_7745c5c3_Input templruntime.GeneratedComponentInput) (templ_7745c5c3_Err error) {
+			templ_7745c5c3_W, ctx := templ_7745c5c3_Input.Writer, templ_7745c5c3_Input.Context
+			templ_7745c5c3_Buffer, templ_7745c5c3_IsBuffer := templruntime.GetBuffer(templ_7745c5c3_W)
+			if !templ_7745c5c3_IsBuffer {
+				defer func() {
+					templ_7745c5c3_BufErr := templruntime.ReleaseBuffer(templ_7745c5c3_Buffer)
+					if templ_7745c5c3_Err == nil {
+						templ_7745c5c3_Err = templ_7745c5c3_BufErr
+					}
+				}()
+			}
+			ctx = templ.InitializeContext(ctx)
+			_, templ_7745c5c3_Err = templ_7745c5c3_Buffer.WriteString("<p class=\"mb-4 lg:mb-8 font-light text-center text-gray-700 dark:text-gray-400 sm:text-xl\">feel free to reach out to me for work related offers or just to have a chat :)</p><div class=\"flex flex-col items-center\"><p class=\"mb-4 lg:mb-8 font-light text-center text-gray-500 dark:text-gray-400 sm:text-xl\">find me on my socials</p><!-- social icons -->")
+			if templ_7745c5c3_Err != nil {
+				return templ_7745c5c3_Err
+			}
+			templ_7745c5c3_Err = components.SocialIcons().Render(ctx, templ_7745c5c3_Buffer)
+			if templ_7745c5c3_Err != nil {
+				return templ_7745c5c3_Err
+			}
+			_, templ_7745c5c3_Err = templ_7745c5c3_Buffer.WriteString("<p class=\"mt-8 lg:mb-8 font-light text-center text-gray-500 dark:text-gray-400 sm:text-xl\">or</p><!-- contacts button --><div class=\"flex flex-wrap gap-4 justify-center\"><a href=\"mailto:janosmolnar1001@gmail.com\" class=\"shadow-xl float-left inline-flex items-start justify-content px-5 py-3 text-base font-medium text-center text-gray-900 border border-gray-300 rounded-lg hover:bg-gray-100 dark:text-white dark:border-gray-700 dark:hover:bg-gray-700 dark:focus:ring-gray-800\">send me an email</a></div></div>")
+			if templ_7745c5c3_Err != nil {
+				return templ_7745c5c3_Err
+			}
 			return templ_7745c5c3_Err
-		}
-		templ_7745c5c3_Err = components.SocialIcons().Render(ctx, templ_7745c5c3_Buffer)
-		if templ_7745c5c3_Err != nil {
-			return templ_7745c5c3_Err
-		}
-		_, templ_7745c5c3_Err = templ_7745c5c3_Buffer.WriteString("<p class=\"mt-8 lg:mb-8 font-light text-center text-gray-500 dark:text-gray-400 sm:text-xl\">or</p><!-- contacts button --><div class=\"flex flex-wrap gap-4 justify-center\"><a href=\"mailto:janosmolnar1001@gmail.com\" class=\"shadow-xl float-left inline-flex items-start justify-content px-5 py-3 text-base font-medium text-center text-gray-900 border border-gray-300 rounded-lg hover:bg-gray-100 dark:text-white dark:border-gray-700 dark:hover:bg-gray-700 dark:focus:ring-gray-800\">send me an email</a></div></div></div></section></div></div></div>")
+		})
+		templ_7745c5c3_Err = components.Container().Render(templ.WithChildren(ctx, templ_7745c5c3_Var2), templ_7745c5c3_Buffer)
 		if templ_7745c5c3_Err != nil {
 			return templ_7745c5c3_Err
 		}
