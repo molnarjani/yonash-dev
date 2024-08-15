dbc4939d81e7f7773c1cda94eecd4c4b4b9dac2b
FormJani <janos.molnar@formlabs.com>
2024-08-16 00:18:01 +0200

    add contacts, share social icons

--- internal/web/templates/pages/contacts_templ.go
+++ internal/web/templates/pages/contacts_templ.go
@@ -8,6 +8,10 @@ package pages
 import "github.com/a-h/templ"
 import templruntime "github.com/a-h/templ/runtime"
 
+import (
+	"github.com/molnarjani/yonash-dev/internal/web/templates/components"
+)
+
 func ContactsPage() templ.Component {
 	return templruntime.GeneratedTemplate(func(templ_7745c5c3_Input templruntime.GeneratedComponentInput) (templ_7745c5c3_Err error) {
 		templ_7745c5c3_W, ctx := templ_7745c5c3_Input.Writer, templ_7745c5c3_Input.Context
@@ -26,6 +30,18 @@ func ContactsPage() templ.Component {
 			templ_7745c5c3_Var1 = templ.NopComponent
 		}
 		ctx = templ.ClearChildren(ctx)
+		_, templ_7745c5c3_Err = templ_7745c5c3_Buffer.WriteString("<div class=\"bg-white\"><div class=\"container mx-auto py-8\"><div class=\"col-span-4 sm:col-span-9\"><section class=\"bg-white dark:bg-gray-900\"><div class=\"py-8 lg:py-16 px-4 mx-auto max-w-screen-md\"><p class=\"mb-4 lg:mb-8 font-light text-center text-gray-700 dark:text-gray-400 sm:text-xl\">feel free to reach out to me for work related offers or just to have a chat :)</p><div class=\"flex flex-col items-center\"><p class=\"mb-4 lg:mb-8 font-light text-center text-gray-500 dark:text-gray-400 sm:text-xl\">find me on my socials</p><!-- social icons -->")
+		if templ_7745c5c3_Err != nil {
+			return templ_7745c5c3_Err
+		}
+		templ_7745c5c3_Err = components.SocialIcons().Render(ctx, templ_7745c5c3_Buffer)
+		if templ_7745c5c3_Err != nil {
+			return templ_7745c5c3_Err
+		}
+		_, templ_7745c5c3_Err = templ_7745c5c3_Buffer.WriteString("<p class=\"mt-8 lg:mb-8 font-light text-center text-gray-500 dark:text-gray-400 sm:text-xl\">or</p><!-- contacts button --><div class=\"flex flex-wrap gap-4 justify-center\"><a href=\"mailto:janosmolnar1001@gmail.com\" class=\"shadow-xl float-left inline-flex items-start justify-content px-5 py-3 text-base font-medium text-center text-gray-900 border border-gray-300 rounded-lg hover:bg-gray-100 dark:text-white dark:border-gray-700 dark:hover:bg-gray-700 dark:focus:ring-gray-800\">send me an email</a></div></div></div></section></div></div></div>")
+		if templ_7745c5c3_Err != nil {
+			return templ_7745c5c3_Err
+		}
 		return templ_7745c5c3_Err
 	})
 }
